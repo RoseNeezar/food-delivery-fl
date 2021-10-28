b6c0517f4ae7068e9892a6c33cdd4947e11c9f14
Neezar <rforlyffe@gmaicl.com>
2021-10-28 23:51:53 +0800

    add side scroller

--- lib/screens/home.screens.dart
+++ lib/screens/home.screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/widgets/recentOrders.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -28,11 +29,32 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30))),
-          )
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(width: 0.8)),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                          width: 0.8, color: Theme.of(context).primaryColor)),
+                  hintText: "Search Food",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30.0,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  )),
+            ),
+          ),
+          RecentOrders()
         ],
       ),
     );
